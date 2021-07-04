b486f0b83f9edd10d575ef7166c2b36841bcd7e5
Andy Gray <30691631+codingWithAndy@users.noreply.github.com>
2021-07-04 12:28:41 +0100

    created results page.

--- Implementation/Web App/app.py
+++ Implementation/Web App/app.py
@@ -51,5 +51,19 @@ def explination():
 
     return render_template('explination.html')
 
+@app.route('/results/', methods=['GET','POST'])
+def results():
+    if request.method == 'GET':
+        pass
+    
+    if request.method == 'POST':
+        name = request.form.get('name')
+        post = request.form.get('post')
+        create_post(name, post)
+
+    #posts = get_post()
+
+    return render_template('results.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
