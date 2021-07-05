314198783447d86547106d26154fa4750af8e943
Andy Gray <30691631+codingWithAndy@users.noreply.github.com>
2021-07-05 22:03:28 +0100

    \n removed from list.

--- Implementation/Web App/logic.py
+++ Implementation/Web App/logic.py
@@ -3,20 +3,27 @@ def get_tweets():
     with open('workfile', 'r') as f:
         lines = [line for line in f.readlines()] # f.readlines()
 
+    stripped_line = [s.rstrip() for s in lines]
+    print(stripped_line)
+    #if len(lines) > 0:
+     #   lines = lines.rstrip()
     print(lines)
     print(len(lines))
+    #for line in lines:
+    
         #data = f.read()
     #    print(lines)
     #    number = lines
     #    print(number)
+    #lines = [lines.replace('\n', '') for x in lines]
     if len(lines) > 0:
             with open('workfile', 'a') as f:
-                f.write("testing123"+",")
-                f.write("\n")
+                f.write("testing123"+"\n")
+                #f.write("\n")
     else:
         with open('workfile', 'a') as f:
-            f.write("testing"+",")
-            f.write("\n")
+            f.write("testing"+"\n")
+            #f.write("\n")
 
 
 # Adding Tweets to Database from CSV idea
