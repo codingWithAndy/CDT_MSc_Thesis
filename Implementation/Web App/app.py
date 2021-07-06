c97f45ac556a241978a7e8accf9fd41bc9fb7a97
Andy Gray <30691631+codingWithAndy@users.noreply.github.com>
2021-07-06 15:43:43 +0100

    comparison development.

--- Implementation/Web App/app.py
+++ Implementation/Web App/app.py
@@ -27,13 +27,19 @@ def index():
 @app.route('/compare/', methods=['GET','POST'])
 def compare():
     if request.method == 'GET':
-        tweets = get_tweets()
+        #tweets = get_tweets()
+        pass
     
     if request.method == 'POST':
-        name = request.form.get('name')
-        post = request.form.get('post')
-        create_post(name, post)
+        radio_1 = request.form.get('radio 1')
+        radio_2 = request.form.get('radio 2')
+        print("content of radio 1 is:", radio_1)
+        print("content of radio 2 is:", radio_2)
+        #post = request.form.get('post')
+        #create_post(name, post)
+        pass
 
+    tweets = get_tweets()
     #posts = get_post()
 
     return render_template('compare.html')
--- Implementation/Web App/logic.py
+++ Implementation/Web App/logic.py
@@ -5,25 +5,19 @@ def get_tweets():
 
     stripped_line = [s.rstrip() for s in lines]
     print(stripped_line)
-    #if len(lines) > 0:
-     #   lines = lines.rstrip()
-    print(lines)
-    print(len(lines))
-    #for line in lines:
-    
-        #data = f.read()
-    #    print(lines)
-    #    number = lines
-    #    print(number)
-    #lines = [lines.replace('\n', '') for x in lines]
-    if len(lines) > 0:
-            with open('workfile', 'a') as f:
-                f.write("testing123"+"\n")
-                #f.write("\n")
+
+    if len(stripped_line) > 0:
+        value = int(stripped_line[-1]) + 1
+        with open('workfile', 'a') as f:
+            f.write(str(value)+"\n")
+            #f.write("\n")
     else:
         with open('workfile', 'a') as f:
-            f.write("testing"+"\n")
+            f.write("1"+"\n")
             #f.write("\n")
+
+    # query database for the records matching value
+
 
 
 # Adding Tweets to Database from CSV idea
