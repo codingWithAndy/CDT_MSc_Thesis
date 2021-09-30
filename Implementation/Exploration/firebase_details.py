6593b55cbbb64040f9b05e42bc382da0f232a389
Andy Gray <30691631+codingWithAndy@users.noreply.github.com>
2021-09-30 20:03:39 +0100

    removing private keys.

---
+++ Implementation/Exploration/firebase_details.py
@@ -0,0 +1,12 @@
+def link_to_firebase():
+    firebase_config = {
+            "apiKey": "AIzaSyAjlnmPp71zTRrXhJ7kun_pzCXnZvLRt3A",
+            "authDomain": "cj-app-e65ce.firebaseapp.com",
+            "databaseURL": "https://cj-app-e65ce-default-rtdb.europe-west1.firebasedatabase.app/",
+            "projectId": "cj-app-e65ce",
+            "storageBucket": "cj-app-e65ce.appspot.com",
+            "messagingSenderId": "407532536815",
+            "appId": "1:407532536815:web:38c8c3ea2726122e0fac1a",
+            "measurementId": "G-PTKRNCZJGB"
+        }
+    return firebase_config
