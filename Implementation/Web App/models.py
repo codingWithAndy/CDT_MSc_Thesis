b30f05ba5ea548e7f7902e735555a3fdcc939613
Andy Gray <30691631+codingWithAndy@users.noreply.github.com>
2021-09-30 20:05:46 +0100

    removing private keys.

--- Implementation/Web App/models.py
+++ Implementation/Web App/models.py
@@ -1,19 +1,13 @@
 import pyrebase
+from firebase_details import link_to_firebase
+
+
 
 
 ###### Firebase Connections ############
 
 def connect_to_firebase():
-    firebase_config = {
-        "apiKey": "AIzaSyAjlnmPp71zTRrXhJ7kun_pzCXnZvLRt3A",
-        "authDomain": "cj-app-e65ce.firebaseapp.com",
-        "databaseURL": "https://cj-app-e65ce-default-rtdb.europe-west1.firebasedatabase.app/",
-        "projectId": "cj-app-e65ce",
-        "storageBucket": "cj-app-e65ce.appspot.com",
-        "messagingSenderId": "407532536815",
-        "appId": "1:407532536815:web:38c8c3ea2726122e0fac1a",
-        "measurementId": "G-PTKRNCZJGB"
-    }
+    firebase_config = link_to_firebase()
 
     firebase = pyrebase.initialize_app(firebase_config)
 
